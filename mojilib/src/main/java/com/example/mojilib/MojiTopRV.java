e8a0fa95bf8bd53e98894d88363281f06d19a5da
Scott Baar <orphanfrequently@gmail.com>
2016-01-09 15:23:13 -0800

    progress

---
+++ mojilib/src/main/java/com/example/mojilib/MojiTopRV.java
@@ -0,0 +1,7 @@
+package com.example.mojilib;
+
+/**
+ * Created by Scott Baar on 1/6/2016.
+ */
+public class MojiTopRV {
+}
---
+++ mojilib/src/main/java/com/example/mojilib/model/Category.java
@@ -0,0 +1,7 @@
+package com.example.mojilib.model;
+
+/**
+ * Created by Scott Baar on 1/9/2016.
+ */
+public class Category {
+}
