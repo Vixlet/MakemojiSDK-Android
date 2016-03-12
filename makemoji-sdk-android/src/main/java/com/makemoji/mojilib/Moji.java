48fbff09a6957edb1d3e737b0a5ed5208d7d926b
Scott Baar <orphanfrequently@gmail.com>
2016-03-12 07:35:43 -0800

    updated retrofit to 2.0

--- makemoji-sdk-android/src/main/java/com/makemoji/mojilib/Moji.java
+++ makemoji-sdk-android/src/main/java/com/makemoji/mojilib/Moji.java
@@ -41,7 +41,7 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.GsonConverterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
 
 import static android.content.Context.ACTIVITY_SERVICE;
--- makemoji-sdk-android/src/main/java/com/makemoji/mojilib/SmallCB.java
+++ makemoji-sdk-android/src/main/java/com/makemoji/mojilib/SmallCB.java
@@ -3,6 +3,7 @@ package com.makemoji.mojilib;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
@@ -12,8 +13,8 @@ import retrofit2.Response;
  */
 public abstract class SmallCB<T> implements Callback<T>{
     @Override
-    public void onResponse(Response<T> response) {
-        if (response.isSuccess()){
+    public void onResponse(Call<T> call,Response<T> response) {
+        if (response.isSuccessful()){
             done(response,null);
         }
         else
@@ -24,7 +25,7 @@ public abstract class SmallCB<T> implements Callback<T>{
     }
 
     @Override
-    public void onFailure(Throwable t) {
+    public void onFailure(Call<T> call,Throwable t) {
         done(null,t);
 
     }
