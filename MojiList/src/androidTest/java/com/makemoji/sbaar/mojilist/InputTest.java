03461b5e1370f1f761475d8092f82d740f97a70d
Scott Baar <scottpbaar@gmail.com>
2017-04-02 03:52:57 -0700

    add tests, update support libs

--- MojiList/src/androidTest/java/com/makemoji/sbaar/mojilist/InputTest.java
+++ MojiList/src/androidTest/java/com/makemoji/sbaar/mojilist/InputTest.java
@@ -2,11 +2,16 @@ package com.makemoji.sbaar.mojilist;
 import static android.support.test.espresso.Espresso.*;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions.*;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.matcher.*;
 
 import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -14,10 +19,17 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
+import com.makemoji.mojilib.Moji;
 import com.makemoji.mojilib.MojiInputLayout;
+import com.makemoji.mojilib.wall.MojiWallActivity;
 
+import android.support.test.InstrumentationRegistry;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,21 +40,64 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @SmallTest
 public class InputTest {
+    final String SPAN_STRING = " \uFFFC ";
     @Rule
     public ActivityTestRule<InputActivity> mActivityRule = new ActivityTestRule<>(
             InputActivity.class);
 
+    public void openKb(){
+        onView(withId(R.id._mm_edit_text)).perform(ViewActions.click(),ViewActions.typeTextIntoFocusedView("ab"),ViewActions.clearText());
+    }
     @Test//test ! appears
     public void testSearchClick(){
-        onView(withId(R.id._mm_edit_text)).perform(ViewActions.click(),ViewActions.typeTextIntoFocusedView("ab"),ViewActions.clearText());//open kb
+        openKb();
         onView(withId(R.id._mm_flashtag_button)).perform(ViewActions.click());
         onView(withId(R.id._mm_edit_text)).check(ViewAssertions.matches(ViewMatchers.withText("!")));
     }
 
     @Test public void testOpenLeft(){
-        onView(withId(R.id._mm_edit_text)).perform(ViewActions.click(),ViewActions.typeTextIntoFocusedView("ab"),ViewActions.clearText());//open kb
+        openKb();
         onView(withId(R.id._mm_horizontal_top_scroller)).perform(ViewActions.swipeRight());
         onView(withId(R.id._mm_categories_button)).perform(ViewActions.click());
         onView(withId(R.id._mm_page_container)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+    //tests handling the wall result intent
+    @Test public void testHandleIntent(){
+        final Intent intent = new Intent();
+        intent.putExtra(Moji.EXTRA_PACKAGE_ORIGIN,InstrumentationRegistry.getTargetContext().getPackageName());
+        intent.putExtra(Moji.EXTRA_JSON,"{\"native\":0,\"emoji\":[],\"flashtag\":\"SWAG\",\"gif\":0,\"id\":1011327," +
+                "\"image_url\":\"https:\\/\\/d1tvcfe0bfyi6u.cloudfront.net\\/emoji\\/1011327-large@2x.png\"," +
+                "\"locked\":false,\"name\":\"SWAG\",\"phrase\":0,\"video\":0,\"video_url\":\"\"}");
+        onView(withId(R.id.mojiInput)).perform(new CustomActionMojiInput(){
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((MojiInputLayout)view).handleIntent(intent);
+            }
+        });
+        onView(withId(R.id._mm_edit_text)).check(ViewAssertions.matches(ViewMatchers.withText(" \uFFFC ")));
+    }
+    //test visibility and focus when attatch/detaching an outside edit text
+    @Test public void testAttatchDetatch(){
+        onView(withId(R.id._mm_edit_text)).perform(ViewActions.click(),ViewActions.typeTextIntoFocusedView("a"));
+        onView(withId(R.id._mm_recylcer_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0,ViewActions.click()));
+        onView(withId(R.id._mm_edit_text)).check(ViewAssertions.matches(ViewMatchers.withText("a" +SPAN_STRING)));
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Attatch EditText")).perform(ViewActions.click());
+
+        onView(withId(R.id.outside_met)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id._mm_edit_text)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.outside_met)).perform(ViewActions.click(),ViewActions.typeTextIntoFocusedView("b"));
+        onView(withId(R.id._mm_recylcer_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0,ViewActions.click()));
+        onView(withId(R.id.outside_met)).check(ViewAssertions.matches(ViewMatchers.withText("b" + SPAN_STRING)));
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Detach EditText")).perform(ViewActions.click());
+
+        onView(withId(R.id._mm_edit_text)).perform(ViewActions.click(),ViewActions.typeTextIntoFocusedView("b"));
+        onView(withId(R.id._mm_recylcer_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0,ViewActions.click()));
+        onView(withId(R.id._mm_edit_text)).check(ViewAssertions.matches(ViewMatchers.withText("a"+SPAN_STRING+"b"+ SPAN_STRING)));
+        onView(withId(R.id.outside_met)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id._mm_edit_text)).check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
     }
 }
