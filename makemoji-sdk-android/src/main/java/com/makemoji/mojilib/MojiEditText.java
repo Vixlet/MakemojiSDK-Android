109fe4a3fd2e1b130a7a60d8fec0cf735f9a8a7a
Scott Baar <scottpbaar@gmail.com>
2017-06-20 00:38:14 -0700

    appcompat moojieditext reflection fix

--- makemoji-sdk-android/src/main/java/com/makemoji/mojilib/MojiEditText.java
+++ makemoji-sdk-android/src/main/java/com/makemoji/mojilib/MojiEditText.java
@@ -265,7 +265,7 @@ public class MojiEditText extends AppCompatEditText implements ISpecialInvalidat
     }
     private void stopActionMode(){
         try{
-            Method m = getClass().getSuperclass().getSuperclass().getDeclaredMethod("stopTextActionMode");
+            Method m = getClass().getSuperclass().getSuperclass().getSuperclass().getDeclaredMethod("stopTextActionMode");
             m.setAccessible(true);
             m.invoke(this);
         }
